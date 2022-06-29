31d543fb75add1ea0eb6a5ccfc87d4934d9fc7b9
Akhil Gandhi <akhilgandhi@live.in>
2022-06-29 16:44:43 +0530

    Added Controller endpoint

--- src/main/java/com/example/springcijenkins/SpringCiJenkinsApplication.java
+++ src/main/java/com/example/springcijenkins/SpringCiJenkinsApplication.java
@@ -2,9 +2,19 @@ package com.example.springcijenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringCiJenkinsApplication {
+
+    @GetMapping
+    public ResponseEntity<String> greeting() {
+        return new ResponseEntity<>("Hello World!!!", HttpStatus.OK);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringCiJenkinsApplication.class, args);
