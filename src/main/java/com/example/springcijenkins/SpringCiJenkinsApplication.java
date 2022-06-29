1af72d24d0682b324fbef6f1b26c24aaf8b0c0ee
Akhil Gandhi <akhilgandhi@live.in>
2022-06-29 16:41:45 +0530

    Initial commit

---
+++ src/main/java/com/example/springcijenkins/SpringCiJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcijenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCiJenkinsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCiJenkinsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springcijenkins/SpringCiJenkinsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springcijenkins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCiJenkinsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
